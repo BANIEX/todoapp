aac7f117b31898a78f4937c94dc2f156f16d884e
baniex <banjibaniex1@gmail.com>
2022-07-04 10:16:30 +0100

    bravo commit

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 window.addEventListener("load", function(){
     let note_app_content = localStorage.getItem("note_app_content");
+    let j = 'hi'
+    
     let array = [];
+    if(note_app_content){
+        note_app_content = JSON.parse(note_app_content);
+        let array_length = note_app_content.length;
+        console.log(array_length);
+        for (let strings of note_app_content){
+            var $newdiv1 = $( `<li class='list-group-item d-flex justify-content-between'> <span class='px-2'>${strings}</span> <input type='checkbox' name='' id=''></li>` );
+
+
+            $(".note-list").append( $newdiv1);
+           
+
+        }
+    }
     document.getElementsByClassName("plus")[0].addEventListener("click", function(){
         let note_text = document.getElementById("note_text").value;
         alert("hi");
@@ -10,6 +25,8 @@ window.addEventListener("load", function(){
             console.log(note_text);
             console.log(array);
             localStorage.setItem("note_app_content", JSON.stringify(array));
+            var $newdiv1 = $( `<li class='list-group-item d-flex justify-content-between'> <span class='px-2'>${note_text}</span> <input type='checkbox' name='' id=''></li>` );
+            $(".note-list").append( $newdiv1);
 
 
         }
@@ -18,7 +35,25 @@ window.addEventListener("load", function(){
             array_from_storage.push(note_text)
             localStorage.setItem("note_app_content", JSON.stringify(array_from_storage));
             console.log(array_from_storage);
+            var $newdiv1 = $( `<li class='list-group-item d-flex justify-content-between'> <span class='px-2'>${note_text}</span> <input type='checkbox' name='' id=''></li>` );
+            $(".note-list").append($newdiv1);
         }
+        if(note_app_content){
+            note_app_content = JSON.parse(note_app_content);
+            let array_length = note_app_content.length;
+            console.log(array_length);
+            for (let strings of note_app_content){
+                var $newdiv1 = $( `<li class='list-group-item d-flex justify-content-between'> <span class='px-2'>${strings}</span> <input type='checkbox' name='' id=''></li>` );
+    
+    
+                $(".note-list").append( $newdiv1);
+               
+    
+            }
+        }
+
+        let currentArray = JSON.parse(localStorage.getItem("note_app_content"));
+        console.log(currentArray.length)
       
        
         
@@ -26,4 +61,6 @@ window.addEventListener("load", function(){
 
     })
     
-})
+});
+
+
